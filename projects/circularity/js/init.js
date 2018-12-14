f6ebe47d8f5ae051ecbab78a14237fc911a6f9dc
William Swafford <wswafford49@gmail.com>
2018-12-14 20:07:14 +0000

    create a visual poem

---
+++ projects/circularity/js/init.js
@@ -0,0 +1,99 @@
+var init = function (window) {
+    'use strict';
+    var 
+        draw = window.opspark.draw,
+        physikz = window.opspark.racket.physikz,
+        
+        app = window.opspark.makeApp(),
+        canvas = app.canvas, 
+        view = app.view,
+        fps = draw.fps('#000');
+        
+    
+    window.opspark.makeRunner = function() {
+        
+        window.opspark.runner = {};
+        var runner = window.opspark.runner;
+        
+        ////////////////////////////////////////////////////////////////
+        // ALL CODE GOES BELOW HERE                                   //
+        ////////////////////////////////////////////////////////////////
+        
+        // TODO 1: Declare our variables //
+        var circle;
+        var circles = [];
+
+        // TODO 2: Draw a circle //
+        var drawCircle = function() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+        circles.push(circle);
+    }
+        
+        // TODO 3 : Call the drawCircle function 5 times //
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+
+        // TODO 7 : Create a Loop to call drawCircle 100 times
+        var counter = 0;
+        while (counter < 100) {
+            drawCircle();
+            counter++;
+        }
+    
+        view.addChild(fps);
+        app.addUpdateable(fps);
+    
+        runner.checkCircleBounds = function(circle) {
+            var areaWidth = canvas.width + circle.radius;
+            var areaHeight = canvas.height + circle.radius;
+            
+            // TODO 5 : YOUR CODE STARTS HERE //////////////////////
+            if ( circle.x > areaWidth ) {
+                circle.x = - circle.radius;
+            } 
+            else if ( circle.x < - circle.radius ) {
+                circle.x = areaWidth;
+            } 
+            if ( circle.y > areaHeight ) {
+                circle.y = - circle.radius;
+            } 
+            else if ( circle.y < - circle.radius ) {
+                circle.y = areaHeight;
+            }
+            
+        };
+    
+        function update() {
+    
+            // TODO 8 : Iterate over the array
+            for (var i = 0; i < circles.length; i++) {
+                var circle = circles[i];
+                physikz.updatePosition(circle);
+                runner.checkCircleBounds(circle);
+            }
+        }
+        
+        ////////////////////////////////////////////////////////////////////
+        // NO CODE BELOW HERE                                             //
+        ////////////////////////////////////////////////////////////////////
+        
+        runner.circle = circle;
+        runner.circles = circles;
+        runner.drawCircle = drawCircle;
+        runner.update = update;
+        
+        app.addUpdateable(window.opspark.runner);
+    };
+};
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports = init;
+}
